95f1d8c06aef94b5418a01fd1def096f03088586
Daria Komarynska <01165657@pw.edu.pl>
2022-05-19 03:21:58 +0200

    Created empty running app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,11 +3,11 @@
 public class App {
 
     public static void main(String[] args) {
-//        WeatherApiServer weatherApiServer = new WeatherApiServer();
-        if (args.length == 3) {
-            System.out.println("Cool");
-        } else {
-            System.out.println("Not valid number of arguments. Try again!");
-        }
+        // if 3 args
+        // if file exists
+        // if city is in the file
+        // if type exists and is daily or hourly
+        // get coordinates
+        WeatherApiServer.run();
     }
 }
--- src/main/java/WeatherApiServer.java
+++ src/main/java/WeatherApiServer.java
@@ -1,7 +1,13 @@
-
+import java.util.ArrayList;
 
 public class WeatherApiServer {
-    public static void run(){
-        System.out.println("run");
+
+    private static String urlString(String city, String weatherType, ArrayList<String> coordinates) {
+        String apiKey = System.getenv("API_KEY");
+        return "url";
+    }
+
+    public static void run() {
+        System.out.println("Weather forecast");
     }
 }
